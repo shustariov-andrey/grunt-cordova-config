d254f00d3b6a983b99d9b55bc8a0da2cbd503579
Andrey Shustariov <shustarev.andrey@gmail.com>
2014-06-11 23:49:41 +0300

    Removed commented out code

--- tasks/cordova_config.js
+++ tasks/cordova_config.js
@@ -100,35 +100,9 @@ module.exports = function (grunt) {
       var result = js2xmlparser("widget", data);
 
       // Write the destination file.
-//      grunt.log.writeln(JSON.stringify(this));
       grunt.file.write(this.data.dest, result);
 
       grunt.log.writeln('File "' + this.data.dest + '" created.');
-//      // Iterate over all specified file groups.
-//      this.files.forEach(function (file) {
-//         // Concat specified files.
-//         var src = file.src.filter(function (filepath) {
-//            // Warn on and remove invalid source files (if nonull was set).
-//            if (!grunt.file.exists(filepath)) {
-//               grunt.log.warn('Source file "' + filepath + '" not found.');
-//               return false;
-//            } else {
-//               return true;
-//            }
-//         }).map(function (filepath) {
-//            // Read file source.
-//            return grunt.file.read(filepath);
-//         }).join(grunt.util.normalizelf(options.separator));
-//
-//         // Handle options.
-//         src += options.punctuation;
-//
-//         // Write the destination file.
-//         grunt.file.write(file.dest, src);
-//
-//         // Print a success message.
-//         grunt.log.writeln('File "' + file.dest + '" created.');
-//      });
    });
 
 };
